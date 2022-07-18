be5746ee20cdb88c775510626be769b324a323a2
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2022-07-17 23:19:32 -0400

    Update prog_7.cpp

--- prog_7.cpp
+++ prog_7.cpp
@@ -1,23 +1,23 @@
 // [7.] C program to calculate factorial of a number.
 
-#include<iostream>
-using namespace std;
-int main()
+#include<iostream>  // Declaration of the standard input output stream header file.
+using namespace std;  // Declaration of the standard namespace.
+int main()  // Main or startup function.
 {
-	int i,n,fact=1;
-	cout<<"Enter any number = ";
-	cin>>n;
+	int i,n,fact=1;  // Declaration of the variables.
+	cout<<"Enter any number = ";  // Prompting the user to enter a number of which we want to find the factorial.
+	cin>>n;  // Inputing the value of the number to the variable.
 	
-	cout<<n<<" !"<<" = ";
-	for(i=n;i>=1;i--)
+	cout<<n<<" !"<<" = "; 
+	for(i=n;i>=1;i--)  // Initializing the for loop in the program.
 	{
-		cout<<i;
+		cout<<i;  // Printing numbers which undergoes factorial multiplication.
 		if(i!=1)
 		{
-			cout<<" * ";	
+			cout<<" * ";	// Logic to print '*' upto 1, while factorising. For example, n! = n * (n - 1) * (n - 2) * .... * 3 * 2 * 1. 
   		}	
-  		fact*=i;
+  		fact*=i;  // Logic for factorisation
 	}
-	cout<<" = "<<fact;
-	return 0;
+	cout<<" = "<<fact;  // Printing the final answer, after the calculation of the factorisation.
+	return 0;  // Returning the value of 0, after the execution of the main function.
 }
