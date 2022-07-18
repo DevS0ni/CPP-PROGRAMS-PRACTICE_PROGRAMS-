caf0d8a51c495923f8cbffe39e7848c2214ebd13
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2022-07-17 22:34:34 -0400

    Update prog_2.cpp

--- prog_2.cpp
+++ prog_2.cpp
@@ -1,13 +1,13 @@
 // [2.] C program to print all alphabets from a to z.
 
-#include<iostream>
-using namespace std;
-int main()
+#include<iostream>  // Declaration of the standard input output stream header file for the C++ program.
+using namespace std;  // Declaration of the standard namespace for the C++ program.
+int main()  // Main or start up function for the program.
 {
-	char ch;
-	for(ch='a';ch<='z';ch++)
+	char ch;  // Declaration of the variables.
+	for(ch='a';ch<='z';ch++)  // Implementation of the for loop.
 	{
-		cout<<ch<<"\t";
+		cout<<ch<<"\t";  // Outputing the letters from 'a' to 'z' in the alphabetical series.
 	}
-	return 0;
+	return 0;  // Returning the value 0, after the completion of the main function.
 }
