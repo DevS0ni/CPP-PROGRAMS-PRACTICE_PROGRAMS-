5d66e839c40dccea1e4fb43bcb207ad362a6f9d5
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2022-07-17 23:38:18 -0400

    Update prog_1.cpp

--- prog_1.cpp
+++ prog_1.cpp
@@ -1,9 +1,9 @@
 // [1.] TO PRINT HELLO WORLD.
 
-#include<iostream>
-using namespace std;
-int main()
+#include<iostream>  // Declaration of standard input output stream header file.
+using namespace std;  // Declaration of the standard namespace, for your program.
+int main()  // Main or start-up function for program.
 {
-	cout<<"Hello World"<<endl;
-	return 0;
+	cout<<"Hello World"<<endl;  // Printing Hello World to the console.
+	return 0;  // Returning the integer value 0, to the system, after executing Main function.
 }
