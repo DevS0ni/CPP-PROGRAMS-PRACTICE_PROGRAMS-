81381288b787cb67500e2cdb8b0f7e0e4571eea1
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2022-07-17 23:45:38 -0400

    Update and rename prog_2.cpp to BackSlashCharacters.cpp

---
+++ BackSlashCharacters.cpp
@@ -0,0 +1,9 @@
+// [2.] PROGRAM TO DISPLAY FOLLOWING OUTPUT USING A SINGLE 'cout' STATEMENT [BACK SLASH CHARACTERS - '\n', '\t'].
+
+#include<iostream>  // Declaration of the standard input output stream header file.
+using namespace std;  // Declaration of the standard namespace for the program.
+int main()  // Main or Start-up function of the program.
+{
+	cout<<"Subject Marks\n\nMathematics\t90\nComputer\t77\nChemistry\t69";  // Printing the information with back slash characters ('\n', '\t', '\r').
+	return 0;  // Returning the integer value 0, to the system, after the execution of the Main function.
+}
--- prog_2.cpp
+++
@@ -1,9 +0,0 @@
-// [2.] PROGRAM TO DISPLAY FOLLOWING OUTPUT USING A SINGLE 'cout' STATEMENT.
-
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout<<"Subject Marks\n\nMathematics\t90\nComputer\t77\nChemistry\t69";
-	return 0;
-}
