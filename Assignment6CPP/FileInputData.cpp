333497f92f023eb1f72161635d4726604fffb07c
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2023-11-16 18:18:15 -0500

    Update FileInputData.cpp

--- Assignment6CPP/FileInputData.cpp
+++ Assignment6CPP/FileInputData.cpp
@@ -1,13 +1,22 @@
 // Write a C++ program to write number 1 to 100 in a data file NOTES.TXT. 
 
-#include<iostream>  // Declaration of the input output stream header file for the C++ program.
-#include<fstream>  // Declaration of the file stream i.e., it has the abilities of both ofstream and ifstream - means it can create files, write information to files, and read information from files.
-using namespace std;  // Declaration of the standard namespace for the program.
-int main(){  // Main function or Startup function invoke in the program.
-	int i;  // Declaration of the variable.
-	fstream f("NOTES.TXT");  // This is to create and open the file named - NOTES.TXT
-	// f.open("NOTES.TXT");
-	f<<"File is Storing !";  // This will be printed in the file.
-	f.close();  // This is to close the created file.
-	return 0;  // Returning the integer value 0, after the execution of the Main function.
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::ofstream outFile("NOTES.TXT");
+
+    if (!outFile) {
+        std::cerr << "Error opening file for writing.\n";
+        return 1;
+    }
+
+    for (int i = 1; i <= 100; ++i) {
+        outFile << i << "\n";
+    }
+
+    std::cout << "Numbers 1 to 100 written to NOTES.TXT successfully.\n";
+
+    outFile.close();
+    return 0;
 }
