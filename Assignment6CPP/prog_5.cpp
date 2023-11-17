821758ccc4a3a2045d19a3abea08ca69b1da8e23
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2023-11-17 09:35:34 -0500

    Update prog_5.cpp

--- Assignment6CPP/prog_5.cpp
+++ Assignment6CPP/prog_5.cpp
@@ -1,1 +1,45 @@
+/*
+[5.] Write a function to count number of words in a text file named "OUT.TXT"
+*/
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+int countWordsInFile(const std::string& fileName) {
+    std::ifstream inFile(fileName);
+
+    if (!inFile.is_open()) {
+        std::cerr << "Error opening file " << fileName << " for reading.\n";
+        return -1; // Return -1 to indicate an error
+    }
+
+    int wordCount = 0;
+    std::string word;
+
+    // Read words from the file
+    while (inFile >> word) {
+        wordCount++;
+    }
+
+    // Close the file
+    inFile.close();
+
+    return wordCount;
+}
+
+int main() {
+    std::string fileName = "OUT.TXT";
+
+    // Call the user-defined function to count words
+    int wordCount = countWordsInFile(fileName);
+
+    if (wordCount != -1) {
+        std::cout << "Number of words in " << fileName << ": " << wordCount << "\n";
+    }
+
+    return 0;
+}
+
+
 
