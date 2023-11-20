768b96437d665be7c6c06c4f5495636f146a3fd4
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2023-11-20 09:26:26 -0500

    Update prog_3.cpp

--- Assignment8CPP/prog_3.cpp
+++ Assignment8CPP/prog_3.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
 
+class Library {
+private:
+    std::string bookName;
+    std::string authorName;
+    float bookPrice;
+
+public:
+    void readInfo() {
+        std::cout << "Enter Book Name: ";
+        std::getline(std::cin, bookName);
+        std::cout << "Enter Author Name: ";
+        std::getline(std::cin, authorName);
+        std::cout << "Enter Book Price: ";
+        std::cin >> bookPrice;
+    }
+
+    void displayInfo() const {
+        std::cout << "Book Name: " << bookName << std::endl;
+        std::cout << "Author Name: " << authorName << std::endl;
+        std::cout << "Book Price: $" << bookPrice << std::endl;
+    }
+};
+
+int main() {
+    Library book1;
+    book1.readInfo();
+    book1.displayInfo();
+
+    return 0;
+}
