551e3cd47048788d1898e1721b47c34ba1abb772
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2023-11-20 09:26:11 -0500

    Update prog_2.cpp

--- Assignment8CPP/prog_2.cpp
+++ Assignment8CPP/prog_2.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <string>
 
+class Employee {
+private:
+    int employeeNo;
+    std::string employeeName;
+    std::string employeeID;
+    double employeeSalary;
+
+public:
+    void readInfo() {
+        std::cout << "Enter Employee Number: ";
+        std::cin >> employeeNo;
+        std::cin.ignore();
+        std::cout << "Enter Employee Name: ";
+        std::getline(std::cin, employeeName);
+        std::cout << "Enter Employee ID: ";
+        std::getline(std::cin, employeeID);
+        std::cout << "Enter Employee Salary: ";
+        std::cin >> employeeSalary;
+    }
+
+    void displayInfo() const {
+        std::cout << "Employee Number: " << employeeNo << std::endl;
+        std::cout << "Employee Name: " << employeeName << std::endl;
+        std::cout << "Employee ID: " << employeeID << std::endl;
+        std::cout << "Employee Salary: $" << employeeSalary << std::endl;
+    }
+};
+
+int main() {
+    Employee employee1;
+    employee1.readInfo();
+    employee1.displayInfo();
+
+    return 0;
+}
