0bc5d98f794a830e44ebec09dd240a6a950dfac7
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2023-11-20 09:26:42 -0500

    Update prog_4.cpp

--- Assignment8CPP/prog_4.cpp
+++ Assignment8CPP/prog_4.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <string>
 
+class Product {
+private:
+    int productNumber;
+    std::string productName;
+    float productPrice;
+    std::string batchNumber;
+
+public:
+    void readInfo() {
+        std::cout << "Enter Product Number: ";
+        std::cin >> productNumber;
+        std::cin.ignore();
+        std::cout << "Enter Product Name: ";
+        std::getline(std::cin, productName);
+        std::cout << "Enter Product Price: ";
+        std::cin >> productPrice;
+        std::cin.ignore();
+        std::cout << "Enter Batch Number: ";
+        std::getline(std::cin, batchNumber);
+    }
+
+    void displayInfo() const {
+        std::cout << "Product Number: " << productNumber << std::endl;
+        std::cout << "Product Name: " << productName << std::endl;
+        std::cout << "Product Price: $" << productPrice << std::endl;
+        std::cout << "Batch Number: " << batchNumber << std::endl;
+    }
+};
+
+int main() {
+    Product product1;
+    product1.readInfo();
+    product1.displayInfo();
+
+    return 0;
+}
