1c55d365ac22aad390cecef858971c04ea57cd26
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2023-11-20 09:36:18 -0500

    Update prog_2.cpp

--- Assignment8CPP/Assign8CPP_Extra/prog_2.cpp
+++ Assignment8CPP/Assign8CPP_Extra/prog_2.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
 
+class Hardware {
+private:
+    char choice;
+    double price;
+
+public:
+    void showPrice() {
+        std::cout << "Choose hardware (F for Floppy, C for CD, P for Pen drive): ";
+        std::cin >> choice;
+
+        switch (choice) {
+            case 'F':
+                price = 15.0;
+                break;
+            case 'C':
+                price = 20.0;
+                break;
+            case 'P':
+                price = 1250.0;
+                break;
+            default:
+                std::cout << "Invalid choice!" << std::endl;
+                return;
+        }
+
+        std::cout << "Price of the chosen hardware: $" << price << std::endl;
+    }
+};
+
+int main() {
+    Hardware hardware;
+    hardware.showPrice();
+
+    return 0;
+}
