c15993737deac7877f100858c1f13fdf1c7952dd
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2023-11-22 00:20:58 -0500

    Update lab1.cpp

--- Extra_C++Labs/Lab1/lab1.cpp
+++ Extra_C++Labs/Lab1/lab1.cpp
@@ -1,1 +1,36 @@
+// Q-1. What is the main difference between C and C++?
+/*
+   Answer: The main difference between C and C++ is that C++ is an extension of C with the addition
+   of object-oriented programming (OOP) features. C++ includes classes and objects, which allow
+   developers to use the concepts of encapsulation, inheritance, and polymorphism. C is a procedural
+   programming language, while C++ supports both procedural and object-oriented programming paradigms.
+*/
+
+// Q-2. What is OOP?
+/*
+   Answer: OOP stands for Object-Oriented Programming. It is a programming paradigm that uses objects,
+   which are instances of classes, for designing and implementing software. OOP focuses on organizing
+   code into reusable and modular structures called classes. It introduces concepts such as
+   encapsulation, inheritance, and polymorphism to improve code structure, reusability, and maintainability.
+*/
+
+// Q-3. What are the advantages and disadvantages of OOP?
+/*
+   Advantages of OOP:
+   - OOP promotes the reuse of code through the concept of classes and objects.
+   - Code is organized into self-contained modules (classes), making it easier to manage.
+   - Data and methods that operate on the data are encapsulated within a class, enhancing
+     data security and preventing unauthorized access.
+   - It allows a new class to inherit properties and behaviors from an existing class,
+     promoting code reuse and extensibility.
+   - It enables objects of different types to be treated as objects of a common base type,
+     providing flexibility in code design.
+
+   Disadvantages of OOP:
+   - OOP can introduce complexity, especially for small and simple programs.
+   - It may take time for programmers to grasp the concepts of OOP, especially if they
+     are transitioning from procedural programming.
+   - OOP may have a slight performance overhead due to the dynamic nature of
+     object-oriented code and additional layers of abstraction.
+*/
 
