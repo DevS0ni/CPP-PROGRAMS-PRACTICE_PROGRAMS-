ef7607829c5f415ed03823009f9cf701c68b03e7
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2023-11-21 23:37:23 -0500

    Update prog_17.cpp

--- Assignment8CPP/Assign8CPP_Extra/prog_17.cpp
+++ Assignment8CPP/Assign8CPP_Extra/prog_17.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+int main() {
+    float arr[3][3] = {
+        {-50.5},
+        {500.1, 70.7},
+        {100.9, 0.5, 20.2}
+    };
+
+    // Display array elements
+    std::cout << "2-D Array Elements:\n";
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    // Display array length
+    std::cout << "\nArray Length: " << sizeof(arr) / sizeof(float) << std::endl;
+
+    return 0;
+}
